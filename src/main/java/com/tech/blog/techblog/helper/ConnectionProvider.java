3b91d92967f179bfac3947e5d22be0fa6a5a8f5c
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2022-07-20 14:25:48 +0530

    added sql connection

--- src/main/java/com/tech/blog/techblog/helper/ConnectionProvider.java
+++ src/main/java/com/tech/blog/techblog/helper/ConnectionProvider.java
@@ -1,2 +1,24 @@
-package com.tech.blog.techblog.helper;public class ConnectionProvider {
+package com.tech.blog.techblog.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+    private  static Connection connection;
+
+    public static Connection getConnection(){
+
+        try{
+            if(connection==null){
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/techblog","root","qwerty@12345");
+
+            }
+        }catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
